2b1568966a7293e7643d5c93c5664ef1b946c8fd
shahriyorbozorov <shahriyorbozorov12@gmail.com>
2023-02-18 15:39:30 +0500

    Created Customer models

---
+++ src/ShammsBusiness.Domain/Common/Audiable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShammsBusiness.Domain.Common
+{
+    public class Audiable : BaseEntity
+    {
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
---
+++ src/ShammsBusiness.Domain/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShammsBusiness.Domain.Models
+{
+    public class Customer
+    {
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string VakilName { get; set; }
+    }
+}
